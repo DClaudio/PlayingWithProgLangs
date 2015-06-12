be7028af8e085b27842604145bc4afa7e18849e4
DClaudio <david_claudio_t@yahoo.com>
2015-06-12 15:25:21 +0100

    bootstrap tutorial done

--- lisp/lisp-capi-gui.lisp
+++ lisp/lisp-capi-gui.lisp
@@ -1,27 +1,36 @@
+;create a test callback and hello function
+
 (defun test-callback (data interface)
   (display-message "Data ~S in interface ~S"
                    data interface))
 (defun hello (data interface)
   (declare (ignore data interface))
-  (display-message "Hellow World"))
+  (display-message "Hello World"))
+;create the CAPI interface with a menu Foo which contains four items
 (make-instance 'menu
-               :title "Foo"
-               :items '("One" "Two" "Three" "Four")
-               :callback 'test-callback)
+    :title "Foo"
+    :items '("One" "Two" "Three" "Four")
+    :callback 'test-callback)
+ 
 (make-instance 'interface
-               :menu-bar-items (list*))
+    :menu-bar-items (list *))
+ 
 (display *)
 
+;; create a submenu by specifying a menu as one of the items of the top-level menu
 (make-instance 'menu
     :title "Bar"
     :items '("One" "Two" "Three" "Four")
     :callback 'test-callback)
+ 
 (make-instance 'menu
-    :title "Baz"
-    :items (list 1 2 * 4 5)
-    :callback 'test-callback)
- 
+    :title "Baz"
+    :items (list 1 2 * 4 5)
+    :callback 'test-callback)
+ 
 (contain *)
+
+;; this is an example of a menu-component class that lets you grou related items together
 (setq component (make-instance 'menu-component
                      :items '("item 1" "item2")
                      :print-function 'string-capitalize
@@ -35,3 +44,27 @@
                         :callback 'hello)
          :width 150
          :height 0)
+
+
+;;RADIO buttons
+(setq radio (make-instance 'menu-component
+                :interaction :single-selection
+                :items '("This" "That")
+                :callback 'hello))
+ 
+(setq commands (make-instance 'menu
+                :title "Commands"
+                :items
+                 (list "Command 1" radio "Command 2")
+                :callback 'test-callback))
+ 
+(contain commands)
+
+;;Checed menu
+(setq letters (make-instance 'menu-component
+                  :interaction :multiple-selection
+                  :items (list "Alpha" "Beta")))
+(contain (make-instance 'menu
+                  :title "Greek"
+                  :items (list letters)
+                  :callback 'test-callback))
